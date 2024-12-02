9c4a8d3842feddf8c230a18e1908e0a5d8158d1b
NhiDinh <thaonhi6991@gmail.com>
2024-12-02 22:41:20 +0200

    add dao.TimeSettingsDao

--- src/main/java/com/simulator/hospital/model/datasource/MariaDbJpaConnection.java
+++ src/main/java/com/simulator/hospital/model/datasource/MariaDbJpaConnection.java
@@ -8,7 +8,6 @@ public class MariaDbJpaConnection {
     private static EntityManager em = null;
 
     public static EntityManager getInstance() {
-        // you need to add synchronization if you run in a multi-threaded environment
 
         if (em==null) {
             if (emf==null) {
