42489991254b8be35f6698d88dca314ce9292ea7
NhiDinh <thaonhi6991@gmail.com>
2024-12-02 20:18:48 +0200

    add entity.ServicePointTypes

---
+++ src/main/java/com/simulator/hospital/model/entity/ServicePointTypes.java
@@ -0,0 +1,36 @@
+package com.simulator.hospital.model.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "servicepointtypes")
+
+public class ServicePointTypes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column (name = "type_name")
+    private String typeName;
+    @Column (name = "number_of_points")
+    private int numberPoints;
+
+    public ServicePointTypes(String typeName, int numberPoints) {
+        this.typeName = typeName;
+        this.numberPoints = numberPoints;
+    }
+
+    public ServicePointTypes() {
+
+    }
+
+    public String getTypeName() {
+        return this.typeName;
+    }
+
+    public int getNumberPoints() {
+        return this.numberPoints;
+    }
+
+    public void setNumberPoints(int numberPoints) {
+        this.numberPoints = numberPoints;
+    }
+}
