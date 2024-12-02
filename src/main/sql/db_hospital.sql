4f149274da9ff7613d284ce8a61cdeddb46fbd97
NhiDinh <thaonhi6991@gmail.com>
2024-12-02 22:29:47 +0200

    fix error for database script

--- src/main/sql/db_hospital.sql
+++ src/main/sql/db_hospital.sql
@@ -1,4 +1,4 @@
-- Drop the database if it already exists
+-- Drop the database if it already exists
 DROP DATABASE IF EXISTS hospital_simulation;
 
 -- Create the database
@@ -17,7 +17,7 @@ CREATE TABLE ServicePointTypes (
 CREATE TABLE Intervals (
     id INT AUTO_INCREMENT PRIMARY KEY,
     type VARCHAR(50) NOT NULL, -- 'arrival' or 'service'
-    category VARCHAR(255) NULL, -- 'Registration', 'General Health Exam', etc., or NULL for global settings
+    category VARCHAR(255) NOT NULL, -- 'Arrival', 'Registration', 'General Health Exam' or 'Specialist'
     time DOUBLE NOT NULL CHECK (time >= 0)
 );
 -- Table: time_settings
