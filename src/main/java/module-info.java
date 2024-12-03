aeef3e267dbb5e226d4ae58fac398ca695cf4f51
NhiDinh <thaonhi6991@gmail.com>
2024-12-03 02:03:00 +0200

    fixed setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,19 @@ module com.simulator.hospital {
     requires javafx.fxml;
     requires java.desktop;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
+    opens com.simulator.hospital;
+    opens com.simulator.hospital.controller;
+    opens com.simulator.hospital.model.dao;
+    opens com.simulator.hospital.model.entity;
+    opens com.simulator.hospital.model.datasource;
 
-    opens com.simulator.hospital to javafx.fxml;
-    opens com.simulator.hospital.controller to javafx.fxml;
     exports com.simulator.hospital.framework;
     exports com.simulator.hospital.model.logic;
+    exports com.simulator.hospital.model.entity;
+    exports com.simulator.hospital.model.dao;
+    exports com.simulator.hospital.model.datasource;
     exports com.simulator.hospital.controller;
     exports com.simulator.hospital.view;
     exports com.simulator.eduni.distributions;
